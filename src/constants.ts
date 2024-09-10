30b0070345f72383ce7f9c7c240fb766fa5e1a47
Sean Huang <castle2668@gmail.com>
2024-09-11 00:05:25 +0800

    Update site config constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
-export const SITE_TITLE = "My Site";
-export const SITE_DESCRIPTION = "My site description";
-export const SITE_URL = "https://www.example.com";
-export const HOMEPAGE_ARTICLE_LIMIT = 5;
-export const ARTICLES_PER_PAGE = 10;
+export const SITE_TITLE = "Sean's Blog";
+export const SITE_DESCRIPTION =
+  "Articles, stories and tutorials about web development, software engineering and programming.";
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
+export const ARTICLES_PER_PAGE = 6;
