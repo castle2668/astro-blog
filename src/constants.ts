81139d13d9280e3a88cef1c8a5346c5b771a83ab
Sean Huang <castle2668@gmail.com>
2024-09-16 01:43:22 +0800

    Add meta tags

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const SITE_TITLE = "Sean's Blog";
 export const SITE_DESCRIPTION =
-  "Articles, stories and tutorials about web development, software engineering and programming.";
+  "Articles, stories, and tutorials for web enthusiasts, experienced developers, a fan of related technologies, or just curious about a technology trend.";
 export const HOMEPAGE_ARTICLE_LIMIT = 6;
 export const ARTICLES_PER_PAGE = 6;
